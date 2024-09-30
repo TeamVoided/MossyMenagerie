b15000a92c7340373590bbf8057bf96fcb87f082
theendercore <theendercore@gmail.com>
2024-09-30 18:02:39 +0300

    - MossyBiomeModifiers

---
+++ src/main/kotlin/org/teamvoided/mossy_menagerie/init/misc/MossyBiomeModifiers.kt
@@ -0,0 +1,23 @@
+package org.teamvoided.mossy_menagerie.init.misc
+
+import net.fabricmc.fabric.api.biome.v1.*
+import net.minecraft.world.gen.GenerationStep
+import org.teamvoided.mossy_menagerie.MossyMenagerie.id
+import java.util.function.Consumer
+import java.util.function.Predicate
+import org.teamvoided.mossy_menagerie.data.world.gen.MossyPlacedFeatures as Mpf
+
+object MossyBiomeModifiers {
+    fun init() {
+        addFeature(
+            "fire_moss",
+            BiomeSelectors.foundInTheNether()
+        ) { it.generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, Mpf.FIRE_MOSS_PATCH) }
+    }
+
+
+    fun addFeature(
+        name: String, selector: Predicate<BiomeSelectionContext>, modifier: Consumer<BiomeModificationContext>
+    ) = BiomeModifications.create(id(name)).add(ModificationPhase.ADDITIONS, selector, modifier)
+
+}
