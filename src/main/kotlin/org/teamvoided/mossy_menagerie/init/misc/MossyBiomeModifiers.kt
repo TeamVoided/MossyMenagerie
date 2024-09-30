9febb0ad244fe1cb78a1eeebdc480dae2558794a
theendercore <theendercore@gmail.com>
2024-09-30 18:45:16 +0300

    - improved MossyBiomeModifiers

--- src/main/kotlin/org/teamvoided/mossy_menagerie/init/misc/MossyBiomeModifiers.kt
+++ src/main/kotlin/org/teamvoided/mossy_menagerie/init/misc/MossyBiomeModifiers.kt
@@ -1,7 +1,9 @@
 package org.teamvoided.mossy_menagerie.init.misc
 
 import net.fabricmc.fabric.api.biome.v1.*
+import net.minecraft.registry.RegistryKey
 import net.minecraft.world.gen.GenerationStep
+import net.minecraft.world.gen.feature.PlacedFeature
 import org.teamvoided.mossy_menagerie.MossyMenagerie.id
 import org.teamvoided.mossy_menagerie.data.tags.MossyBiomeTags
 import java.util.function.Consumer
@@ -10,19 +12,18 @@ import org.teamvoided.mossy_menagerie.data.world.gen.MossyPlacedFeatures as Mpf
 
 object MossyBiomeModifiers {
     fun init() {
-        addFeature(
-            "fire_moss",
-            BiomeSelectors.foundInTheNether()
-        ) { it.generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, Mpf.FIRE_MOSS_PATCH) }
-        addFeature(
-            "fire_moss",
-            BiomeSelectors.tag(MossyBiomeTags.HAS_DARK_MOSS)
-        ) { it.generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, Mpf.FIRE_MOSS_PATCH) }
+        addVegetalDeco("fire_moss", BiomeSelectors.foundInTheNether(), Mpf.FIRE_MOSS_PATCH)
+        addVegetalDeco("fire_moss", BiomeSelectors.tag(MossyBiomeTags.HAS_DARK_MOSS), Mpf.DARK_MOSS_PATCH)
     }
 
+    fun addVegetalDeco(name: String, selector: Predicate<BiomeSelectionContext>, feature: RegistryKey<PlacedFeature>) =
+        addFeature(name, selector) {
+            it.generationSettings.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, feature)
+        }
 
     fun addFeature(
         name: String, selector: Predicate<BiomeSelectionContext>, modifier: Consumer<BiomeModificationContext>
-    ) = BiomeModifications.create(id(name)).add(ModificationPhase.ADDITIONS, selector, modifier)
+    ): BiomeModification =
+        BiomeModifications.create(id(name)).add(ModificationPhase.ADDITIONS, selector, modifier)
 
 }
